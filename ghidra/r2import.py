baccd3e4252341787328c710a9b9867762e8ccdd
Adrian Studer <github@adrianstuder.com>
2021-01-20 16:45:43 -0800

    reformat flag comments, remove unused int vectors

--- ghidra/r2import.py
+++ ghidra/r2import.py
@@ -1,6 +1,7 @@
 #Imports labels and comments from r2 scripts - part of Inside-EZRadioPRO project
 #
-# This script does not replace function names unless they start with 'FUN_'
+# This script does not replace function names and labels unless their names start
+# with 'FUN_' or 'DAT_'.
 #
 # For more information about see:
 # https://github.com/astuder/Inside-EZRadioPRO/ghidra
@@ -11,13 +12,18 @@
 #@menupath 
 #@toolbar 
 
-def setComment(space, addr, comment):
+def setComment(space, addr, comment, type = ''):
     addrobj = toAddr('{}:{:X}'.format(space, addr))
     if addrobj is None:
         # invalid address
         return
     codeUnit = currentProgram.getListing().getCodeUnitAt(addrobj)
-    codeUnit.setComment(codeUnit.EOL_COMMENT, comment)
+    if type == 'PLATE':
+        codeUnit.setComment(codeUnit.PLATE_COMMENT, comment)
+    elif type == 'PRE':
+        codeUnit.setComment(codeUnit.PRE_COMMENT, comment)
+    else:
+        codeUnit.setComment(codeUnit.EOL_COMMENT, comment)
     print('comment @ {:04x}'.format(addr))
     return
 
@@ -88,25 +94,30 @@ def import_func(tokens):
     addr = int(tokens[1], 16)
     name = tokens[3][:-1]
     addLabel('RAM', addr, name, True)
-    return
+    return addr
 
 def import_flag(line):
     addr_str = line.split('@')[1]
     space, addr = parse_addr(addr_str)
     if space is None or addr is None:
-        return
+        return None
     name = line.split(' ')[1]
     addLabel(space, addr, name)
-    return
+    return addr
 
-def import_comment(line):
+def import_comment(line, prev_type, prev_addr):
     splitline = line.split('@')
     addr_str = splitline[1]
     space, addr = parse_addr(addr_str)
     if space is None or addr is None:
         return
     comment = splitline[0][4:].rstrip()
-    setComment(space, addr, comment)
+    if prev_type == 'fcn' and prev_addr == addr:
+        setComment(space, addr, comment)    # no PLATE as most are just basic code comments
+    elif prev_type == 'flag' and prev_addr == addr:
+        setComment(space, addr, comment, 'PRE')
+    else:
+        setComment(space, addr, comment)
     return
 
 try:
@@ -119,16 +130,22 @@ with open(fname, 'r') as script:
     funcs = 0
     flags = 0
     comments = 0
+    prev_addr = -1
+    prev_type = ''
     for line in script:
         line = line.rstrip()
         tokens = line.split(' ')
         if tokens[0] == '.(fcn':
-            import_func(tokens)
+            prev_addr = import_func(tokens)
+            prev_type = 'fcn'            
             funcs += 1
         elif tokens[0] == 'f':
-            import_flag(line)
+            prev_addr = import_flag(line)
+            prev_type = 'flag'
             flags += 1
         elif tokens[0] == 'CCu':            
-            import_comment(line)
-            comments += 1            
+            import_comment(line, prev_type, prev_addr)
+            prev_addr = -1
+            prev_type = ''
+            comments += 1
     print('Processed {} functions, {} flags and {} comments'.format(funcs, flags, comments))
